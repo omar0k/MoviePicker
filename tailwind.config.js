ab93b478623d58b2508c3e8afc5569353c6974c0
omark01 <omark012000@gmail.com>
2023-10-04 16:18:37 -0400

    switching to use TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    fontFamily: {
+      awesome: ['"Font Awesome 5 Free"', "sans"],
+    },
+
+    extend: {
+      colors: {
+        primary: "#9CA8F8", // Primary accent color
+        secondary: "#CDFCF6", // Secondary accent color
+        darkBg: "#343541", // Background color for dark themes
+        lightBg: "#FAF7F0",
+      },
+    },
+  },
+  plugins: [],
+};
